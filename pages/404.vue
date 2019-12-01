dbc866a82c55ae519a0f07dc8c91a35cff1b1648
Michael Lynch <mtlynch@users.noreply.github.com>
2019-12-01 12:48:23 -0600

    Removing rewrite rules for firebase (#9)
    * Removing rewrite rules for firebase
    
    I suspect they're conflicting with proper 404 handling.
    
    * Replacing error layout with a 404 page

---
+++ pages/404.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <h1>Page not found</h1>
+    <p>Sorry, we couldn't find the page you were looking for.</p>
+  </div>
+</template>
+
+<script>
+export default {
+  head: {
+    title: 'Page Not Found',
+  },
+};
+</script>
