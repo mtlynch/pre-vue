a2c1b870a0a19d97bff76324ed415faae08c968c
Michael Lynch <mtlynch@users.noreply.github.com>
2019-12-01 14:58:43 -0600

    Fixing link attribute in nuxt config (#15)
    It's an attribute of the head attribute.

--- nuxt.config.js
+++ nuxt.config.js
@@ -8,14 +8,14 @@ export default {
       {charset: 'utf-8'},
       {name: 'viewport', content: 'width=device-width, initial-scale=1.0'},
     ],
+    link: [
+      {
+        rel: 'icon',
+        type: 'image/x-icon',
+        href: 'favicon.ico',
+      },
+    ],
   },
-  link: [
-    {
-      rel: 'icon',
-      type: 'image/x-icon',
-      href: 'favicon.ico',
-    },
-  ],
   modules: ['@nuxtjs/sitemap'],
   sitemap: {
     hostname: 'https://hello-world-vue-static.web.app/',
