65b982cf439cd9d4698a4dc1f630cf77dba5ca6e
Michael Lynch <mtlynch@users.noreply.github.com>
2019-12-01 14:10:18 -0600

    Adding favicon (#14)

--- nuxt.config.js
+++ nuxt.config.js
@@ -9,6 +9,13 @@ export default {
       {name: 'viewport', content: 'width=device-width, initial-scale=1.0'},
     ],
   },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/x-icon',
+      href: 'favicon.ico',
+    },
+  ],
   modules: ['@nuxtjs/sitemap'],
   sitemap: {
     hostname: 'https://hello-world-vue-static.web.app/',
