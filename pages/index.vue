8ea46fe28b06f7cf24599d8ee7cc8e5e242f2347
Michael Lynch <git@mtlynch.io>
2019-12-11 12:09:02 -0500

    Updating links on main pages

--- pages/index.vue
+++ pages/index.vue
@@ -3,6 +3,10 @@
     <h1>Hello, world!</h1>
     <p>I'm an example of a pre-rendered Vue webpage.</p>
     <p>
+      I automatically generate a <a href="/robots.txt">robots.txt file</a> and
+      <a href="/sitemap.xml">sitemap</a> on every build.
+    </p>
+    <p>
       To learn more, visit my
       <a href="https://github.com/mtlynch/pre-vue">Github repo</a>.
     </p>
